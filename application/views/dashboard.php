efbb52e22952bd042e87095e8aac8392d54c35c0
Victor Gomes <vsampaio@pm.me>
2019-05-24 15:58:35 -0300

    Setting up the environment

---
+++ application/views/dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>MyCMS</title>
+  <link rel="stylesheet" href="<?=base_url('assets/css/main.css')?>">
+</head>
+<body>
+  <h1>MyCSM</h1>
+</body>
+</html>
