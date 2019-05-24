bcc1938d57b04a4fb267d1a73fecaf572301188a
Victor Gomes <vsampaio@pm.me>
2019-05-24 17:01:16 -0300

    Starting the front-end

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -6,8 +6,77 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>MyCMS</title>
   <link rel="stylesheet" href="<?=base_url('assets/css/main.css')?>">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
 </head>
 <body>
-  <h1>MyCSM</h1>
+
+  <aside>
+    <div class='head-menu'>
+      <h3>MyCMS</h3>
+    </div>
+
+    <div class="user-holder">
+      <div class="icon-holder">
+          <i class="material-icons">person</i>
+      </div>
+      <p>Usuário Logado</p>
+    </div>
+    <hr>
+    <div class="menu">
+      <ul>
+        <li>Usuários</li>
+        <li>Cabeçalho</li>
+        <li>Páginas</li>
+        <li>Rodapé</li>
+      </ul>
+    </div>
+    <hr>
+
+    <div class="footer-menu">
+      <ul>
+        <li>Sair</li>
+      </ul>
+    </div>
+  </aside>
+
+  <section id='content' class='content'>
+
+    <div class="card">
+      <div class="icon-holder">
+        icon
+      </div>
+      <h4>Usuários</h4>
+      <p>Administrar usuários do sistema</p>
+      <button class='btn-orange'>Acessar</button>
+    </div>
+
+    <div class="card">
+      <div class="icon-holder">
+        icon
+      </div>
+      <h4>Cabeçalho</h4>
+      <p>Alterar textos e informações do cabeçalho.</p>
+      <button class='btn-orange'>Acessar</button>
+    </div>
+
+    <div class="card">
+      <div class="icon-holder">
+        icon
+      </div>
+      <h4>Páginas</h4>
+      <p>Adicionar e removar páginas do site</p>
+      <button class='btn-orange'>Acessar</button>
+    </div>
+
+    <div class="card">
+      <div class="icon-holder">
+        icon
+      </div>
+      <h4>Rodapé</h4>
+      <p>Alterar textos e informações do rodapé</p>
+      <button class='btn-orange'>Acessar</button>
+    </div>
+
+  </section>
 </body>
 </html>
