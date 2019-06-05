948e4ecf133e6dbd30679c59c2137335749fe351
Victor Gomes <vsampaio@pm.me>
2019-06-05 15:03:34 -0300

    Finishing dashboard and starting the inside pages

---
+++ application/views/Users/index.php
@@ -0,0 +1,10 @@
+<?php $this->load->view('commons/menu'); ?>
+
+<section>
+  <div class="container">
+    <div class="row">
+      <h3 class='title'>Usuários</h3>
+      <a href="#" class='btn-orange-outline'>Novo</a>
+    </div>    
+  </div>
+</section>
--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,74 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>MyCMS</title>
-  <link rel="stylesheet" href="<?=base_url('assets/css/main.css')?>">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
-<body>
 
-  <aside>
-    <div class='head-menu'>
-      <a href="#" class='btn-menu oculto'>
-        <i class="material-icons">menu</i>
-      </a>
-      <h3>MyCMS</h3>
-    </div>
 
-    <div class="user-holder">
-      <div class="icon-holder">
-          <i class="material-icons">person</i>
-      </div>
-      <p>Usuário Logado</p>
-    </div>
-    <hr>
-    <div class="menu">
-      <ul>
-        <li>
-          <a href="">
-            <i class="material-icons">person</i>
-            Usuários
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <i class="material-icons">edit</i>
-            Cabeçalho
-          </a>
-        </li>
-        <li>
-          <a href="#" class="ativo">
-            <i class="material-icons">insert_drive_file</i>
-            Páginas
-          </a>
-        </li>
-        <li>
-          <a href="">
-            <i class="material-icons">edit</i>
-            Rodapé
-          </a>
-        </li>
-      </ul>
-    </div>    
+  <?php $this->load->view('commons/menu'); ?>
 
-    <!-- <hr> -->
-
-    <div class="footer-menu">
-      <ul>
-        <li>
-          <a href="">
-            <i class="material-icons">power_settings_new</i>
-            Sair
-          </a>
-        </li>
-      </ul>
-    </div>    
-  </aside>
-
-  <section id='content' class='content'>
+  <section id='cards'>
 
     <div class="card">
       <div class="icon-holder">
@@ -76,7 +10,7 @@
       </div>
       <h4>Usuários</h4>
       <p>Administrar usuários do sistema</p>
-      <a href="#" class='btn-orange'>Acessar</a>
+      <a href="<?=base_url('Pages/usersIndex')?>" class='btn-orange'>Acessar</a>
     </div>
 
     <div class="card">
@@ -107,7 +41,3 @@
     </div>
 
   </section>
-
-  <script src="<?=base_url('/assets/js/main.js')?>"></script>
-</body>
-</html>
