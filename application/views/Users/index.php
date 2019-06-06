449d2a651362c6066a0c2d287178b2f05df3b75c
Victor Gomes <vsampaio@pm.me>
2019-06-06 13:30:26 -0300

    creating and styling html tables

--- application/views/Users/index.php
+++ application/views/Users/index.php
@@ -5,6 +5,78 @@
     <div class="row">
       <h3 class='title'>Usuários</h3>
       <a href="#" class='btn-orange-outline'>Novo</a>
-    </div>    
+    </div> 
+
+    <div class="wrapper">
+      <table class ='table'>
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Nível</th>
+            <th scope="col">Ações</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>01</td>
+            <td>Victor</td>
+            <td>Admin</td>
+            <td>
+              <div class="dropdown">
+                <span class="dropdown-button">Ações ▼</span>
+                <div class="dropdown-button-content">
+                  <a href="#">Editar</a>
+                  <a href="#">Excluir</a>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>02</td>
+            <td>Victor</td>
+            <td>Admin</td>
+            <td>
+              <div class="dropdown">
+                <span class="dropdown-button">Ações ▼</span>
+                <div class="dropdown-button-content">
+                  <a href="#">Editar</a>
+                  <a href="#">Excluir</a>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>03</td>
+            <td>Victor</td>
+            <td>Admin</td>
+            <td>
+              <div class="dropdown">
+                <span class="dropdown-button">Ações ▼</span>
+                <div class="dropdown-button-content">
+                  <a href="#">Editar</a>
+                  <a href="#">Excluir</a>
+                </div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>04</td>
+            <td>Victor</td>
+            <td>Admin</td>
+            <td>
+              <div class="dropdown">
+                <span class="dropdown-button">Ações ▼</span>
+                <div class="dropdown-button-content">
+                  <a href="#">Editar</a>
+                  <a href="#">Excluir</a>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>   
+    </div>
   </div>
 </section>
