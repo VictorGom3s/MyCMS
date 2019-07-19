ac806f23e354dc7e707f22831578d35fe12d38e1
Victor <vsampaio@pm.me>
2019-07-19 16:02:59 -0300

    Hiding my personal info

--- application/config/email.php
+++ application/config/email.php
@@ -3,8 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'ssl://smtp.googlemail.com';
-$config['smtp_user'] = 'victorgom3s@gmail.com';
-$config['smtp_pass'] = 'ssjqcxnijkgxsxni';
+
 $config['smtp_port'] = '465';
 $config['smtp_timeout'] = '60';
 $config['charset'] = 'utf-8'; // Default should be utf-8 (this should be a text field) 
